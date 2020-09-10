8d9018fff898c1cc03ee1a6fcc09164203eb4465
theblakeyg <theblakeyg@gmail.com>
2020-09-09 22:54:45 -0500

    Added Get Actions to the CelestialObjectController

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -12,6 +13,73 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                var co = _context.CelestialObjects.FirstOrDefault(o => o.Id == id);
+
+                if (co == null)
+                {
+                    return NotFound();
+                }
+
+                co.Satellites = _context.CelestialObjects.Where(o => o.OrbitedObjectId == id).ToList();
+
+                return Ok(co);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            try
+            {
+                var cos = _context.CelestialObjects.Where(o => o.Name == name);
+
+                if (cos.Count() == 0)
+                {
+                    return NotFound();
+                }
+
+                foreach (var co in cos)
+                {
+                    co.Satellites = _context.CelestialObjects.Where(o => o.OrbitedObjectId == co.Id).ToList();
+                }                
+
+                return Ok(cos);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var cos = _context.CelestialObjects;
+
+                foreach (var co in cos)
+                {
+                    co.Satellites = _context.CelestialObjects.Where(o => o.OrbitedObjectId == co.Id).ToList();
+                }
+
+                return Ok(cos);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
         }
     }
 }
